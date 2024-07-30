d9c1efd80118bb7f409d694cbf0e7b389997a081
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-07-30 11:34:08 +0300

    test(inheritance): Refactor edge cases
    Tests for edge cases are now nested and names for the rest of tests were
    added to give a clear description of what it is expected.

--- app/src/test/java/org/oop/InheritanceTest.java
+++ app/src/test/java/org/oop/InheritanceTest.java
@@ -25,7 +25,7 @@ public class InheritanceTest {
   }
 
   @Nested
-  @DisplayName("Tests constructor")
+  @DisplayName("Constructors")
   class Constructors {
     @Test
     @DisplayName("Should create a Manager object")
@@ -47,62 +47,87 @@ public class InheritanceTest {
   }
 
   @Nested
-  @DisplayName("Should return correct values for manager properties")
+  @DisplayName("Property type")
   class PropertyTypes {
     @Test
+    @DisplayName("Should have name of type String")
     void testManagerNameType() {
       assertInstanceOf(String.class, manager.getName(), "Name should be of type String");
     }
 
     @Test
+    @DisplayName("Should have age of type Integer")
     void testManagerAgeType() {
       assertInstanceOf(Integer.class, manager.getAge(), "Age should be of type Integer");
     }
 
     @Test
+    @DisplayName("Should have salary of type Double")
     void testManagerYearlySalaryType() {
       assertInstanceOf(Double.class, manager.calculateYearlySalary(), "Yearly salary should be of type Double");
     }
 
     @Test
+    @DisplayName("Should have bonus of type Double")
     void testManagerBonusType() {
       assertInstanceOf(Double.class, manager.getBonus(), "Bonus should be of type Double");
     }
   }
 
   @Nested
+  @DisplayName("Setters and getters")
   class PropertySetters {
     @Test
+    @DisplayName("Should set a new name for the manager")
     void testSetName() {
       manager.setName("Mary Doe");
       assertEquals("Mary Doe", manager.getName(), "Name should be 'Mary Doe'");
     }
 
     @Test
+    @DisplayName("Should set a new age for the manager")
     void testSetAge() {
       manager.setAge(40);
       assertEquals(40, manager.getAge(), "Age should be 40");
     }
 
     @Test
+    @DisplayName("Should set a new salary for the manager")
     void testSetSalary() {
       manager.setSalary(3000);
       assertEquals(3000, manager.getSalary(), "Salary should be 3000");
     }
 
     @Test
+    @DisplayName("Should set a new bonus for the manager")
     void testSetBonus() {
       manager.setBonus(200);
       assertEquals(200, manager.getBonus(), "Bonus should be 200");
     }
   }
 
-  @Test
-  @DisplayName("Should not allow nagative values for object properties")
-  void testNegativeValues() {
-    assertAll("Should not allow negative values for object properties",
-      () -> assertThrows(IllegalArgumentException.class, () -> manager.setAge(-1), "Should throw exception for negative age"),
-      () -> assertThrows(IllegalArgumentException.class, () -> manager.setSalary(-1), "Should throw exception for negative salary"),
-      () -> assertThrows(IllegalArgumentException.class, () -> manager.setBonus(-1), "Should throw exception for negative bonus"));
+  @Nested
+  @DisplayName("Edge cases")
+  class EdgeCases {
+    @Test
+    @DisplayName("Should not set a negative age for the manager")
+    void testSetNegativeAge() {
+      assertThrows(IllegalArgumentException.class, () -> manager.setAge(-1),
+          "Should throw exception for negative age");
+    }
+
+    @Test
+    @DisplayName("Should not set a negative salary for the manager")
+    void testSetNegativeSalary() {
+      assertThrows(IllegalArgumentException.class, () -> manager.setSalary(-1),
+          "Should throw exception for negative salary");
+    }
+
+    @Test
+    @DisplayName("Should not set a negative bonus for the manager")
+    void testSetNegativeBonus() {
+      assertThrows(IllegalArgumentException.class, () -> manager.setBonus(-1),
+          "Should throw exception for negative bonus");
+    }
   }
 }
