5256ebd011df0f9bf2f5936512de0db94bc15b4c
Yerold <112283864+yeroldsan@users.noreply.github.com>
2024-08-07 14:08:28 +0900

    test(BubbleSort): Change class name
    This change is required to match the one given to the file and be
    accessible by gradle test command since the class is public now.

--- app/src/test/java/org/sorting/BubbleSortTest.java
+++ app/src/test/java/org/sorting/BubbleSortTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 
 @DisplayName("BubbleSort class tests")
-public class BubbleSortingTest {
+public class BubbleSortTest {
 
   @Test
   @DisplayName("Should sort the array in ascending order") 
